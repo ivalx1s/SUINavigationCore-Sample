b92c7a59964230295387b459db62581101a46bf5
Ivan Oparin <oparin@me.com>
2025-08-22 17:36:29 +0400

    Initial Commit

---
+++ SUINavigationCore-Sample/SUINavigationCore_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUINavigationCore_SampleApp.swift
+//  SUINavigationCore-Sample
+//
+//  Created by Ivan Wb on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SUINavigationCore_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
